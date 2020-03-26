2c4db8ba2e891c4f88a1d72391f6a7a63d9ab082
CiaraMaria Proia <proiacm@gmail.com>
2020-03-26 16:27:10 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,6 @@
-def my_collect(array)
-i = 0
-while i < array.length
-  yield(array[i])
-i = i + 1
-end
-end
+def my_collect(collection)
 
-my_collect(array) {|name| name.split("").first}
+
+
+
+end
