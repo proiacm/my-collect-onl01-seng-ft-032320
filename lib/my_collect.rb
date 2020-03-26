281a273d2a51a2bac0c702c63fabc3ce8d549c98
CiaraMaria Proia <proiacm@gmail.com>
2020-03-26 16:32:41 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,8 @@
 def my_collect(collection)
 i = 0
 new_collection = []
-while i < array.length
- new_collection << yield(array[i])
+while i < collection.length
+ new_collection << yield(collection[i])
 i = i + 1
 end
 collection
